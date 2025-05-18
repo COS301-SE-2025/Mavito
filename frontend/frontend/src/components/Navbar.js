8cd0b971ff251951857058629688257c626567b5
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2025-05-18 17:32:04 +0200

    Setup component file structure
    Added initial component file for stub.

---
+++ frontend/frontend/src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+import dataManager from "../utils/dataManager";
+
+ export class NavBar extends React.Component{
+     constructor(props) {
+         super(props);
+     }
+
+     render(){
+      return(
+          <nav>
+          </nav>
+      );
+  }
+}
+
+
